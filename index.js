61a4eef23160c4cca739c3e544d252e1770e1d58
zidan_1013 <80787878+zidan4@users.noreply.github.com>
2025-03-04 20:13:47 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
+import mongoose from "mongoose";
+
+const MONGO_URI = process.env.MONGO_URI;
+
+export const connectDB = async () => {
+  if (mongoose.connections[0].readyState) return;
+  try {
+    await mongoose.connect(MONGO_URI);
+    console.log("🔥 MongoDB Connected");
+  } catch (error) {
+    console.error("❌ MongoDB Connection Error:", error);
+  }
+};
 
